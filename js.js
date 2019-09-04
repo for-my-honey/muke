89d2f986c84171f5c2ea4be6abf8977f7bed53d0
for-my-honey <1255044799@qq.com>
2019-09-04 15:42:49 +0800

    first commit

---
+++ js.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2019/7/13.
+ */
+// window.onload=function () {
+//     var oMes = document.getElementById('message');
+//     var oLi = oMes.getElementsByTagName('li');
+//     for (var  i=0;i<oLi.length;i++){
+//         oLi.onmouseover=function () {
+//             for(var i=0;i<oLi.length;i++){
+//                 oLi.className='';
+//             }
+//         }
+//     }
+// };
